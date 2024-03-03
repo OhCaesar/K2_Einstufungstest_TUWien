49d84d86d2f643b1aaad8bc0e23e34c6454ca1dc
OhCaesar <cscode.philipp@gmail.com>
2024-03-03 20:04:53 +0100

    Corrected CharArray Mistake

--- src/main/java/at/TuWien/ExampleTest.java
+++ src/main/java/at/TuWien/ExampleTest.java
@@ -14,11 +14,10 @@ public class ExampleTest {
     public static char findMaxChar(String text){
 
         //Umschreiben without CharacterArray
-        char[] charArray = text.toCharArray();
-        char highestChar=charArray[0];
-        for (char character : charArray)
-            if(character>highestChar)
-                highestChar=character;
+        char highestChar=text.charAt(0);
+        for (int i = 0;i<text.length();i++)
+            if(text.charAt(i)>highestChar)
+                highestChar=text.charAt(i);
         return highestChar;
     }
 
